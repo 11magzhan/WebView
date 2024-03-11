5fde26d1afc8f61d6ada2c62e635169ffc193a36
Magzhan <11magzhan@gmail.com>
2024-03-11 11:19:04 +0500

    search work

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.webkit.WebViewClient
+import com.example.webview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
+        setContentView(binding.root)
+
+        binding.btnSearch.setOnClickListener {
+            val url = binding.etLink.text.toString()
+            webViewSetup(url)
+        }
     }
+
+    private fun webViewSetup(url: String) {
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.loadUrl("https://google.com/search?q=$url")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
