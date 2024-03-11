078641901c1c54188bb785fc3689495bee6520f9
Magzhan <11magzhan@gmail.com>
2024-03-11 12:28:59 +0500

    menu land work

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.webview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import com.example.webview.databinding.ActivityMainBinding
+
+private const val SEARCH_TAG = "https://google.com/search?q=%d"
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +29,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun webViewSetup(url: String) {
         binding.webView.webViewClient = WebViewClient()
-        binding.webView.loadUrl("https://google.com/search?q=$url")
+        binding.webView.loadUrl("$SEARCH_TAG$url")
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                val newTitle = view.title
+                supportActionBar?.title = newTitle
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -34,5 +45,33 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.color1 -> {
+                binding.btnSearch.setBackgroundColor(getColor(R.color.green))
+                true
+            }
+            R.id.color2 -> {
+                binding.btnSearch.setBackgroundColor(getColor(R.color.blue))
+                true
+            }
+            R.id.color3 -> {
+                binding.btnSearch.setBackgroundColor(getColor(R.color.violet))
+                true
+            }
+            R.id.color1_land -> {
+                binding.btnSearch.setBackgroundColor(getColor(R.color.brown))
+                true
+            }
+            R.id.color2_land -> {
+                binding.btnSearch.setBackgroundColor(getColor(R.color.yellow))
+                true
+            }
+            R.id.color3_land -> {
+                binding.btnSearch.setBackgroundColor(getColor(R.color.orange))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
